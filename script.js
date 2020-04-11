3491333e1cea0146269e75dbb7722d0e49c4b70c
mumushy <mumushy850421@gmail.com>
2020-04-11 20:13:47 +0800

    init

--- script.js
+++ script.js
@@ -14,14 +14,22 @@ function startVideo() {
     err => console.error(err)
   )
 }
-
+var now_mood = "neutral"
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
+  var nowexpressions 
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    try{
+        //console.log(detections[0]['expressions'])
+        get_now_expressions(detections[0])
+    }catch{
+        
+  }
+    
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
@@ -29,3 +37,43 @@ video.addEventListener('play', () => {
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
   }, 100)
 })
+function get_now_expressions(detection_object){
+    expression_list = detection_object['expressions']
+    netural_score = expression_list['neutral']
+    happy_score = expression_list['happy']
+    sad_score = expression_list['sad']
+    angry_score = expression_list['angry']
+    fearful_score = expression_list['fearful']
+    disgusted_score = expression_list['disgusted']
+    surprised_score = expression_list['surprised']
+    array = []
+    array.push(netural_score,happy_score,sad_score,angry_score,fearful_score,disgusted_score,surprised_score)
+    console.log(array)
+    var indexOfMaxValue = array.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+    console.log(indexOfMaxValue)
+    switch(indexOfMaxValue){
+        case 0:
+            now_mood = "netural"
+            break
+        case 1:
+            now_mood = "happy"
+            break
+        case 2:
+            now_mood = "sad"
+            break
+        case 3:
+            now_mood = "angry"
+            break
+        case 4:
+            now_mood = "fearful"
+            break
+        case 5:
+            now_mood = "disgusted"
+            break
+        case 6:
+            now_mood = "surprised"
+            break
+        
+    }
+    console.log(now_mood)
+}
